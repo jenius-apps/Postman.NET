dfe57f8524fe5d76b5929fac26c974429bbaecee
Daniel Paulino <d_paulino@outlook.com>
2020-04-26 14:39:44 -0700

    Added support for string or string[] formdata src property.

--- src/Collections/Models/FormData.cs
+++ src/Collections/Models/FormData.cs
@@ -1,4 +1,7 @@
-﻿namespace Postman.NET.Collections.Models
+﻿using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace Postman.NET.Collections.Models
 {
     public class FormData
     {
@@ -10,7 +13,29 @@
 
         public string Type { get; set; }
 
-        public string[] Src { get; set; }
+        /// <summary>
+        /// String list of file paths.
+        /// </summary>
+        public object Src
+        {
+            get
+            {
+                if (_src is JArray a)
+                {
+                    return a.Select(x => x.ToString()).ToArray();
+                }
+                else if (_src is string s)
+                {
+                    return new string[] { s };
+                }
+                else
+                {
+                    return _src;
+                }
+            }
+            set => _src = value;
+        }
+        private object _src;
 
         public bool Disabled { get; set; }
     }
---
+++ src/Tests/Models/DeserializationTests.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using Postman.NET.Collections.Models;
+using Xunit;
+
+namespace Tests.Models
+{
+    public class DeserializationTests
+    {
+        [Theory]
+        [InlineData("{\"src\":[\"/C:/path/to/file.json\",\"/C:/path/to/file.json\"]}")]
+        [InlineData("{\"src\":[]}")]
+        [InlineData("{\"src\":\"/C:/path/to/file.json\"}")]
+        public void FormDataSrc(string json)
+        {
+            var item = JsonConvert.DeserializeObject<FormData>(json);
+            Assert.True(item.Src is string[]);
+        }
+    }
+}
